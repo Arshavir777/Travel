72b2623e65f43eafa117ea4bebbc08f049a14231
Arshavir <arshogharibyan@gmail.com>
2019-06-16 00:32:04 +0400

    Mdb

---
+++ src/i18n.js
@@ -0,0 +1,15 @@
+import { configure, init, getLocale } from 'i18n';
+
+configure({
+  // setup some locales - other locales default to en silently
+  locales:[ 'am', 'en', 'de'],
+  register: global,
+
+  // where to store json files - defaults to './locales' relative to modules directory
+  directory: __dirname + '/locales',
+  
+  defaultLocale: 'en',
+  
+  // sets a custom cookie name to parse locale settings from  - defaults to NULL
+  cookie: 'lang',
+});
--- src/server.js
+++ src/server.js
@@ -1,18 +1,46 @@
 import express from 'express'
-import ejs from 'ejs'
+import i18n from 'i18n'
+import ejs from './i18n'
+const MongoClient = require('mongodb').MongoClient;
+
 const app = express()
 const port = (process.env.PORT || 5000)
+const uri = "mongodb+srv://arshavir:9mywhJTYX48nVVk@cluster0-lokck.mongodb.net/test?retryWrites=true&w=majority";
+
+const Mclient = new MongoClient(uri, { 
+    useNewUrlParser: true,
+    reconnectTries: Number.MAX_VALUE,
+    reconnectInterval: 1000
+ });
+Mclient.connect((err, client) => {
+    if(err){
+        console.log("ERROR MONGO DB !!!")
+    }else{
+         app.locals.db = client.db("test")
+         app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+    }
+    
+});
+
 
 app.set("view engine", "ejs");
 app.set('views', __dirname + '/views')
+app.use(i18n.init)
 
 var publicDir = require('path').join(__dirname,'/public');
 app.use(express.static(publicDir));
 
-app.get('/', (req, res) => {
-    res.render('index.ejs',{
-        text:'Welcome to my Page'
-    })
-})
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/', (req, res) => {
+    const collection = app.locals.db.collection('user')
+    collection.findOne({email:req.query.email}, (error, response) => {
+        if(error){
+            console.log(error)
+        }
+        res.render('index.ejs',{
+            name: response !== null? response.name: "No data found!",
+            value: req.query.email
+        })
+    })
+ 
+})
