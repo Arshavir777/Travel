a0bd8e6e7db0e95ab133482450318fed55a81550
Arshavir <arshogharibyan@gmail.com>
2019-06-16 01:00:56 +0400

    Mdb

--- src/server.js
+++ src/server.js
@@ -1,27 +1,28 @@
 import express from 'express'
+import ip from 'ip'
 import i18n from 'i18n'
 import ejs from './i18n'
 const MongoClient = require('mongodb').MongoClient;
 
 const app = express()
 const port = (process.env.PORT || 5000)
-const uri = (process.env.MONGO_URL);
+const uri = (process.env.MONGO_URL || "mongodb+srv://arshavir:9mywhJTYX48nVVk@cluster0-lokck.mongodb.net/test?retryWrites=true&w=majority");
 
-const Mclient = new MongoClient(uri, { 
-    useNewUrlParser: true,
-    reconnectTries: Number.MAX_VALUE,
-    reconnectInterval: 1000
- });
-Mclient.connect((err, client) => {
-    if(err){
-        console.log(err.message)
-    }else{
-         app.locals.db = client.db("test")
-         app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-    }
+// const Mclient = new MongoClient(uri, { 
+//     useNewUrlParser: true,
+//     reconnectTries: Number.MAX_VALUE,
+//     reconnectInterval: 1000
+//  });
+// Mclient.connect((err, client) => {
+//     if(err){
+//         console.log(err.message)
+//     }else{
+//          app.locals.db = client.db("test")
+//          app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+//     }
     
-});
-
+// });
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 app.set("view engine", "ejs");
 app.set('views', __dirname + '/views')
@@ -32,15 +33,15 @@ app.use(express.static(publicDir));
 
 
 app.get('/', (req, res) => {
-    const collection = app.locals.db.collection('user')
-    collection.findOne({email:req.query.email}, (error, response) => {
-        if(error){
-            console.log(error)
-        }
+    // const collection = app.locals.db.collection('user')
+    // collection.findOne({email:req.query.email}, (error, response) => {
+    //     if(error){
+    //         console.log(error)
+    //     }
         res.render('index.ejs',{
-            name: response !== null? response.name: "No data found!",
-            value: req.query.email
+            name: ip.address(),
+            value: 78
         })
-    })
+    //})
  
 })
