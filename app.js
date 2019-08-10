c20d0e573c453688ad30541139f0343af68570f5
Arshavir <Arshavir>
2019-08-10 18:22:50 +0400

    66

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 import express from 'express'
 import path from 'path'
-import ip from 'ip'
-import docxParser from 'docx-parser'
 import i18n from 'i18n'
 import cookieParser from 'cookie-parser'
 import bodyParser from 'body-parser'
 import './i18n'
-import fs from 'fs'
 const mongoose = require('mongoose');
 const tourRouter = require( './routes/tours')
 const indexRouter = require( './routes/index')
@@ -18,19 +15,17 @@ mongoose.connect(uri, { useNewUrlParser: true } )
 app.use(bodyParser.json())
 app.use(cookieParser())
 app.use(i18n.init)
-
+//
+app.set("view engine", "ejs")
+app.set('views', __dirname + '/views')
+//
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
 app.use('/css', express.static(__dirname + '/node_modules/lightbox2/dist/css')); // redirect CSS bootstrap
 app.use('/js', express.static(__dirname + '/node_modules/lightbox2/dist/js/')); // redirect CSS bootstrap
-
-
-
-//
-
 app.use(express.static(path.join(__dirname, 'public')));
-
+//
 app.use(function (req, res, next) {
     res.setLocale(req.cookies.lng || 'am')
     res.locals = {
@@ -39,12 +34,7 @@ app.use(function (req, res, next) {
     next();
  });
 app.use('/tour', tourRouter)
-
-app.set("view engine", "ejs")
-app.set('views', __dirname + '/views')
-
 app.use('/', indexRouter)
 app.get('/gallery', (req, res) => { res.render('gallery.ejs') })
 app.get('/about', (req, res) => { res.render('about.ejs') })
-
-app.listen(port, () => console.log(`Example app on port ${port}!`)) 
+app.listen(port, () => console.log(`Express server listening on port ( ${port} ) `)) 
