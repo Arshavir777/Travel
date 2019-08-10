3b92996c1d321b7961ff411c88f7bc35e4703561
Arshavir <Arshavir>
2019-08-10 17:57:22 +0400

    FOX

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 import express from 'express'
 import path from 'path'
+import ip from 'ip'
+import docxParser from 'docx-parser'
 import i18n from 'i18n'
 import cookieParser from 'cookie-parser'
 import bodyParser from 'body-parser'
 import './i18n'
+import fs from 'fs'
 const mongoose = require('mongoose');
 const tourRouter = require( './routes/tours')
 const indexRouter = require( './routes/index')
@@ -12,19 +15,21 @@ const port = (process.env.PORT || 3000)
 const uri = process.env.MONGO_URL || "mongodb+srv://arshavir:9mywhJTYX48nVVk@cluster0-lokck.mongodb.net/travel?retryWrites=true&w=majority"
 mongoose.connect(uri, { useNewUrlParser: true } )
 //
-app.set("view engine", "ejs")
-app.set('views', __dirname + '/views')
-//
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json())
 app.use(cookieParser())
 app.use(i18n.init)
-//
+
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
 app.use('/css', express.static(__dirname + '/node_modules/lightbox2/dist/css')); // redirect CSS bootstrap
 app.use('/js', express.static(__dirname + '/node_modules/lightbox2/dist/js/')); // redirect CSS bootstrap
+
+
+
+//
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function (req, res, next) {
     res.setLocale(req.cookies.lng || 'am')
@@ -35,10 +40,11 @@ app.use(function (req, res, next) {
  });
 app.use('/tour', tourRouter)
 
-
+app.set("view engine", "ejs")
+app.set('views', __dirname + '/views')
 
 app.use('/', indexRouter)
 app.get('/gallery', (req, res) => { res.render('gallery.ejs') })
 app.get('/about', (req, res) => { res.render('about.ejs') })
 
-app.listen(port, () => console.log(`Express server listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app on port ${port}!`)) 
